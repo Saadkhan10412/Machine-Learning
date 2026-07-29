c788ac1ad707cac253490e85e38e3c4dc58f0fad
Saadkhan10412 <saadkhan10412@gmail.com>
2026-07-29 21:47:01 +0530

    deleting extra files

--- file.py
+++
@@ -1,7 +0,0 @@
-while(True):
-    a=input("Input: ")
-    if ((a**q)//8==1):
-        print("prime")
-    else:
-        print("not prime")
-;
--- py_sample.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-X=[1,2,3,4,5]
-Y=[1,4,9,16,25]
-plt.plot(X,Y)
-plt.show()
